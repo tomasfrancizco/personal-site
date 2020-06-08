fc1fc56f88f7ec2af0b5f26514d1935920fedc35
Tomas <tomasfreire@gmail.com>
2020-06-08 17:32:43 -0300

    added octocat images to about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,75 +1,136 @@
 import React from "react"
-import profilePic from "../images/tomasFreire.png"
+// import profilePic from "../images/tomasFreire.png"
+import profilePic2 from "../images/tomasFreire2.png"
+// import profilePic3 from "../images/babyBoss.png"
 
-const About = () => (
-  <div>
-    <div className="section" id="cover">
-      <img src={profilePic} alt="TomasFreire" />
-      <div id="cover-info">
-        <h1 style={{textAlign: "center"}}>Tomás Freire Knight</h1>
-        <p>MERN Stack Web Developer</p>
-        <ul id="social-list">
-          <li>
-            <a href="https://github.com/tomasfrancizco" target="_blank">
-              <img src="../../icons/github.png" alt="github" title="github" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/tomas-f-freire-knight-376b0219/"
-              target="_blank"
-            >
-              <img src="../../icons/linkedin.png" alt="linkedin" title="linkedin" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/tomasfrancizco/" target="_blank">
-              <img src="../../icons/instagram.png" alt="instagram" title="instagram" />
-            </a>
-          </li>
-          {/* <li>
-            <a href="mailto:tomasfreire@gmail.com" target="_blank">
-              <img src="../../icons/gmail.png" alt="gmail" title="gmail" />
-            </a>
-          </li>
-          <li>
-            <a href="https://wa.link/x0nbkf" target="_blank">
-              <img src="../../icons/whatsapp.png" alt="whatsapp" title="whatsapp" />
-            </a>
-          </li> */}
-        </ul>
+import nyantocat from "../images/octodex/nyantocat.gif"
+import codercat from "../images/octodex/codercat.jpg"
+import eclipseCat from "../images/octodex/total-eclipse-of-the-octocat.jpg"
+import original from "../images/octodex/original.png"
+import defunktocat from "../images/octodex/defunktocat.png"
+import hipster from "../images/octodex/hipster-partycat.jpg"
+import linktocat from "../images/octodex/linktocat.jpg"
+import diaDeMuertos from "../images/octodex/octocat-de-los-muertos.jpg"
+import oktobercat from "../images/octodex/oktobercat.png"
+import orderedList from "../images/octodex/orderedlistocat.png"
+import plumber from "../images/octodex/plumber.jpg"
+import homercat from "../images/octodex/homercat.png"
+import droidtocat from "../images/octodex/droidtocat.png"
+import thanktocat from "../images/octodex/thanktocat.png"
+import visionary from "../images/octodex/visionary.jpg"
+import daftpunktocatguy from "../images/octodex/daftpunktocat-guy.gif"
+import daftpunkttocatthomas from "../images/octodex/daftpunktocat-thomas.gif"
+import mummytocat from "../images/octodex/mummytocat.gif"
+
+const About = () => {
+  const octoFiles = [
+    nyantocat,
+    codercat,
+    original,
+    eclipseCat,
+    defunktocat,
+    hipster,
+    linktocat,
+    diaDeMuertos,
+    oktobercat,
+    orderedList,
+    plumber,
+    homercat,
+    droidtocat,
+    thanktocat,
+    visionary,
+    daftpunktocatguy,
+    daftpunkttocatthomas,
+    mummytocat,
+  ]
+  const octoImg = octoFiles[Math.floor(Math.random() * octoFiles.length)]
+
+  return (
+    <div>
+      <div className="section" id="cover">
+        <img src={profilePic2} alt="TomasFreire" />
+        <div id="cover-info">
+          <h1 style={{ textAlign: "center" }}>Tomás Freire Knight</h1>
+          <p>MERN Stack Web Developer</p>
+          <ul id="social-list">
+            <li>
+              <a href="https://github.com/tomasfrancizco" target="_blank">
+                <img src="../../icons/github.png" alt="github" title="github" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/tomas-f-freire-knight-376b0219/"
+                target="_blank"
+              >
+                <img
+                  src="../../icons/linkedin.png"
+                  alt="linkedin"
+                  title="linkedin"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/tomasfrancizco/"
+                target="_blank"
+              >
+                <img
+                  src="../../icons/instagram.png"
+                  alt="instagram"
+                  title="instagram"
+                />
+              </a>
+            </li>
+            {/* <li>
+              <a href="mailto:tomasfreire@gmail.com" target="_blank">
+                <img src="../../icons/gmail.png" alt="gmail" title="gmail" />
+              </a>
+            </li>
+            <li>
+              <a href="https://wa.link/x0nbkf" target="_blank">
+                <img src="../../icons/whatsapp.png" alt="whatsapp" title="whatsapp" />
+              </a>
+            </li> */}
+          </ul>
+        </div>
+      </div>
+      <div className="section" id="about">
+        <img src={octoImg} alt="codercat" />
+        <div>
+          <h3 className="section-title">About</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis
+            vestibulum diam, nec placerat dolor. Morbi porta, lacus vitae
+            pharetra facilisis, felis augue placerat enim, ac tempus mi augue
+            vehicula odio. Suspendisse nec neque pharetra, hendrerit justo
+            elementum, venenatis tellus. Sed egestas, turpis a luctus aliquet,
+            erat ligula viverra nunc, a porttitor urna ipsum ut nisi. Fusce et
+            libero eget metus ullamcorper varius. Suspendisse potenti. Donec
+            lobortis, eros et tristique posuere, turpis ante pellentesque dui,
+            sit amet placerat erat tellus ac purus. Praesent eu ante malesuada
+            nibh volutpat porttitor vel eu mi.<br></br>
+            <br />
+            Pellentesque erat massa, mollis et hendrerit non, tempor vel velit.
+            Maecenas semper libero sit amet turpis pharetra, eu viverra sapien
+            luctus. Phasellus eu metus aliquam, semper augue sed, varius odio.
+            Aliquam dignissim posuere ante sit amet vestibulum. Aenean nec massa
+            risus. Pellentesque semper tincidunt eleifend. Ut rhoncus rutrum
+            sollicitudin. Aliquam eget nisl justo. Nulla ultricies commodo ipsum
+            rutrum fermentum. Cras ex eros, euismod id tempus ut, imperdiet a
+            ipsum. Quisque viverra pharetra sapien, et ullamcorper dui rutrum
+            eget. Fusce leo lacus, facilisis nec mi ac, varius dictum dui. Nunc
+            malesuada, lacus vitae egestas mollis, sem justo euismod mi, vel
+            auctor nibh nisi non lorem. Fusce nec libero in ipsum pharetra
+            eleifend quis hendrerit metus. Sed consequat lorem vel libero
+            ullamcorper elementum. Nam blandit massa vel augue semper ultricies.
+            Pellentesque quis massa ut tellus dignissim interdum quis et tellus.
+            Morbi ac metus ac velit tempus euismod vel id nisi.
+          </p>
+        </div>
       </div>
     </div>
-    <div className="section" id="about">
-      <h3 className="section-title">About</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis
-        vestibulum diam, nec placerat dolor. Morbi porta, lacus vitae pharetra
-        facilisis, felis augue placerat enim, ac tempus mi augue vehicula odio.
-        Suspendisse nec neque pharetra, hendrerit justo elementum, venenatis
-        tellus. Sed egestas, turpis a luctus aliquet, erat ligula viverra nunc,
-        a porttitor urna ipsum ut nisi. Fusce et libero eget metus ullamcorper
-        varius. Suspendisse potenti. Donec lobortis, eros et tristique posuere,
-        turpis ante pellentesque dui, sit amet placerat erat tellus ac purus.
-        Praesent eu ante malesuada nibh volutpat porttitor vel eu mi.<br></br><br/>
-        Pellentesque erat massa, mollis et hendrerit non, tempor vel velit.
-        Maecenas semper libero sit amet turpis pharetra, eu viverra sapien
-        luctus. Phasellus eu metus aliquam, semper augue sed, varius odio.
-        Aliquam dignissim posuere ante sit amet vestibulum. Aenean nec massa
-        risus. Pellentesque semper tincidunt eleifend. Ut rhoncus rutrum
-        sollicitudin. Aliquam eget nisl justo. Nulla ultricies commodo ipsum
-        rutrum fermentum. Cras ex eros, euismod id tempus ut, imperdiet a ipsum.
-        Quisque viverra pharetra sapien, et ullamcorper dui rutrum eget. Fusce
-        leo lacus, facilisis nec mi ac, varius dictum dui. Nunc malesuada, lacus
-        vitae egestas mollis, sem justo euismod mi, vel auctor nibh nisi non
-        lorem. Fusce nec libero in ipsum pharetra eleifend quis hendrerit metus.
-        Sed consequat lorem vel libero ullamcorper elementum. Nam blandit massa
-        vel augue semper ultricies. Pellentesque quis massa ut tellus dignissim
-        interdum quis et tellus. Morbi ac metus ac velit tempus euismod vel id
-        nisi.
-      </p>
-    </div>
-  </div>
-)
+  )
+}
 
 export default About
--- src/components/education.js
+++ src/components/education.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { Card, Button } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 
 import ironhack from "../images/ironhack.jpg"
+import uces from "../images/uces.jpg"
 
 const Education = () => (
   <div className="section" id="education">
@@ -13,6 +14,18 @@ const Education = () => (
           <Card.Title>Full Stack Developer</Card.Title>
           <Card.Subtitle>@ <a href="https://www.ironhack.com/en" target="_blank">Ironhack Mexico City</a></Card.Subtitle><br></br>
           <Card.Subtitle>2019</Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: "18rem" }} className="card">
+        <Card.Img variant="top" src={uces} />
+        <Card.Body>
+          <Card.Title>Advertising</Card.Title>
+          <Card.Subtitle>@ <a href="https://www.uces.edu.ar/" target="_blank">UCES</a></Card.Subtitle><br></br>
+          <Card.Subtitle>2007 - 2011</Card.Subtitle>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
