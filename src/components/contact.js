b16873ee220d8c707397c18e1fc06e6bf5f08948
Tomas <tomasfreire@gmail.com>
2020-06-22 18:00:29 -0300

    added functionality to contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,18 +5,18 @@ const Contact = () => (
   <div className="section" id="contact">
     <h3>CONTACT</h3>
     <h6>Let's work together!</h6>
-    <Form>
+    <Form className="contact-form" method="post" action="https://getform.io/f/035d54f4-515a-461b-8cee-2a2ece721769">
       <Form.Group>
         <Form.Label>Full name</Form.Label>
-        <Form.Control type="text" placeholder="Enter your name" />
+        <Form.Control type="text" placeholder="Enter your name" name="name" id="name"/>
       </Form.Group>
       <Form.Group>
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control type="email" placeholder="name@example.com" name="email" id="email"/>
       </Form.Group>
       <Form.Group>
         <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" rows="3" />
+        <Form.Control as="textarea" rows="3" name="message" id="message"/>
       </Form.Group>
       <Button type="submit" variant="primary">
         Send Message
