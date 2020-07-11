19a545c950f2e4ac449de9cdd03efa77a3934d4e
Tomas <tomasfreire@gmail.com>
2020-07-11 19:48:46 -0300

    style updates and sidebar hover functionality

--- src/components/common/sidebar.js
+++ src/components/common/sidebar.js
@@ -10,7 +10,7 @@ const Header = () => {
       : (list.style.opacity = "0")
     list.style.display == "none"
       ? (list.style.display = "block")
-      : list.style.display = "none"
+      : (list.style.display = "none")
   }
 
   return (
@@ -59,72 +59,74 @@ const Header = () => {
           </Link>
         </ul>
       </div>
-      <div id="sidebar">
-        <div className="tooltips">
-          <Link to="/#cover" className="sidebar-link">
-            <img src="../../icons/about.png" alt="about" />
-          </Link>
-          <span className="tooltiptext">About</span>
-        </div>
-        <div className="tooltips">
-          <Link to="/#work" className="sidebar-link">
-            <img src="../../icons/work.png" alt="work" />
-          </Link>
-          <span className="tooltiptext">Work</span>
-        </div>
-        <div className="tooltips">
-          <Link to="/#education" className="sidebar-link">
-            <img src="../../icons/education.png" alt="education" />
-          </Link>
-          <span className="tooltiptext">Education</span>
-        </div>
-        <div className="tooltips">
-          <Link to="/#skills" className="sidebar-link">
-            <img src="../../icons/skills.png" alt="skills" />
-          </Link>
-          <span className="tooltiptext">Skills</span>
-        </div>
-        <div className="tooltips">
-          <Link to="/#projects" className="sidebar-link">
-            <img src="../../icons/projects.png" alt="projects" />
-          </Link>
-          <span className="tooltiptext">Projects</span>
-        </div>
-        <div className="tooltips">
-          <Link to="/#languages" className="sidebar-link">
-            <img src="../../icons/languages.png" alt="languages" />
-          </Link>
-          <span className="tooltiptext">Languages</span>
-        </div>
-        <div className="tooltips">
-          <Link to="/#achievements" className="sidebar-link">
-            <img src="../../icons/achievements.png" alt="achievements" />
-          </Link>
-          <span className="tooltiptext">Achievements</span>
-        </div>
-        <div className="tooltips">
-          <Link to="/#certifications" className="sidebar-link">
-            <img src="../../icons/certifications.png" alt="certifications" />
-          </Link>
-          <span className="tooltiptext">Certification</span>
-        </div>
-        <div className="tooltips">
-          <Link to="/#photography" className="sidebar-link">
-            <img src="../../icons/photography.png" alt="photography" />
-          </Link>
-          <span className="tooltiptext">Photography</span>
-        </div>
-        <div className="tooltips">
-          <Link to="/#resume" className="sidebar-link">
-            <img src="../../icons/resume.png" alt="resume" />
-          </Link>
-          <span className="tooltiptext">Resume</span>
-        </div>
-        <div className="tooltips">
-          <Link to="/#contact" className="sidebar-link">
-            <img src="../../icons/contact.png" alt="contact" />
-          </Link>
-          <span className="tooltiptext">Contact</span>
+      <div id="sidebar-cont">
+        <div id="sidebar">
+          <div className="tooltips">
+            <Link to="/#cover" className="sidebar-link">
+              <img src="../../icons/about.png" alt="about" />
+            </Link>
+            <span className="tooltiptext">About</span>
+          </div>
+          <div className="tooltips">
+            <Link to="/#work" className="sidebar-link">
+              <img src="../../icons/work.png" alt="work" />
+            </Link>
+            <span className="tooltiptext">Work</span>
+          </div>
+          <div className="tooltips">
+            <Link to="/#education" className="sidebar-link">
+              <img src="../../icons/education.png" alt="education" />
+            </Link>
+            <span className="tooltiptext">Education</span>
+          </div>
+          <div className="tooltips">
+            <Link to="/#skills" className="sidebar-link">
+              <img src="../../icons/skills.png" alt="skills" />
+            </Link>
+            <span className="tooltiptext">Skills</span>
+          </div>
+          <div className="tooltips">
+            <Link to="/#projects" className="sidebar-link">
+              <img src="../../icons/projects.png" alt="projects" />
+            </Link>
+            <span className="tooltiptext">Projects</span>
+          </div>
+          <div className="tooltips">
+            <Link to="/#languages" className="sidebar-link">
+              <img src="../../icons/languages.png" alt="languages" />
+            </Link>
+            <span className="tooltiptext">Languages</span>
+          </div>
+          <div className="tooltips">
+            <Link to="/#achievements" className="sidebar-link">
+              <img src="../../icons/achievements.png" alt="achievements" />
+            </Link>
+            <span className="tooltiptext">Achievements</span>
+          </div>
+          <div className="tooltips">
+            <Link to="/#certifications" className="sidebar-link">
+              <img src="../../icons/certifications.png" alt="certifications" />
+            </Link>
+            <span className="tooltiptext">Certification</span>
+          </div>
+          <div className="tooltips">
+            <Link to="/#photography" className="sidebar-link">
+              <img src="../../icons/photography.png" alt="photography" />
+            </Link>
+            <span className="tooltiptext">Photography</span>
+          </div>
+          <div className="tooltips">
+            <Link to="/#resume" className="sidebar-link">
+              <img src="../../icons/resume.png" alt="resume" />
+            </Link>
+            <span className="tooltiptext">Resume</span>
+          </div>
+          <div className="tooltips">
+            <Link to="/#contact" className="sidebar-link">
+              <img src="../../icons/contact.png" alt="contact" />
+            </Link>
+            <span className="tooltiptext">Contact</span>
+          </div>
         </div>
       </div>
     </div>
