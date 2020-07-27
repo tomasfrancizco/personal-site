132d456f3d1081f893d10f337927c5f21b33c5d1
Tomas <tomasfreire@gmail.com>
2020-07-27 12:53:46 -0300

    baby steps in making dark mode toggle work

--- src/components/common/dark-mode.js
+++ src/components/common/dark-mode.js
@@ -2,35 +2,34 @@ import React from "react"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
 
 class DarkMode extends React.Component {
-  // state = {
-  //   nightMode: () => window.localStorage.theme == "dark" ? "on" : "off"
-  // }
+  state = {
+    condition: false,
+  }
 
-  toggleDark = () => {
-    // let { nightMode } = this.state
-    // const ball = document.getElementById("ball")
-    // if (nightMode == "dark") {
-    //   ball.setAttribute("class", "light")
-    //   this.setState({ nightMode: "off"})
-    // } else {
-    //   ball.setAttribute("class", "light off")
-    //   this.setState({ nightMode: "on"})
-    // }
+  handleClick = () => {
+    this.setState({
+      condition: !this.state.condition,
+    })
   }
 
   render() {
     return (
       <ThemeToggler>
-        {({ theme, toggleTheme }) => (
+        {({ theme, toggleTheme, state }) => (
           <div className="darkmode">
-            <div id="toggle-container" onClick={this.toggleDark}>
-              <div id="ball" className="light"></div>
+            <div
+              id="toggle-container"
+              onClick={
+                (this.handleClick,
+                () => toggleTheme(theme === "light" ? "dark" : "light"))
+              }
+            >
+              <div
+                id="ball"
+                className="light"
+                className={this.state.condition ? "light off" : "light"}
+              ></div>
             </div>
-            <img
-              src="../../../icons/day-night.svg"
-              alt="day-night"
-              onClick={() => toggleTheme(theme === "light" ? "dark" : "light")}
-            />
           </div>
         )}
       </ThemeToggler>
