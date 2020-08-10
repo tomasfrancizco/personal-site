eeb2682b3a399e20869b7942b6c49c7d815d6bdd
Tomas <tomasfreire@gmail.com>
2020-08-10 14:30:46 -0300

    fixed dark mode toggle button bug... finally

--- src/components/common/dark-mode.js
+++ src/components/common/dark-mode.js
@@ -1,29 +1,41 @@
 import React from "react"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
+import sun from "../../images/sun.svg"
+import moon from "../../images/moon.svg"
 
 class DarkMode extends React.Component {
+  componentDidMount() {
+    this.setTheme()
+  }
+
+  setTheme = () => {
+    return document.body.setAttribute("class", "light")
+  }
+
   render() {
     return (
       <ThemeToggler>
-        {({ theme, toggleTheme }) => (
-          <div>
-            <div
-              className="darkmode"
-              onClick={() => {
-                toggleTheme(theme === "light" ? "dark" : "light")
-              }}
-              role="button"
-              tabIndex="-1"
-            >
-              <div id="toggle-container">
-                <div id="ball" className={theme}></div>
+        {({ theme, toggleTheme }) => {
+          return (
+            <div>
+              <div
+                className="darkmode"
+                onClick={() => {
+                  toggleTheme(theme === "light" ? "dark" : "light")
+                }}
+                role="button"
+                tabIndex="-1"
+              >
+                <div id="toggle-container">
+                  <div id="ball">
+                    {" "}
+                    <img src={theme === "dark" ? moon : sun} alt="darkmode" />
+                  </div>
+                </div>
               </div>
             </div>
-            <span className="darkmode-tooltip">
-              {theme === "dark" ? "DARK" : "LIGHT"}
-            </span>
-          </div>
-        )}
+          )
+        }}
       </ThemeToggler>
     )
   }
