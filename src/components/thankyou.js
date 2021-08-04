a475f0221da787d91a35279ca45d32a571c5b998
Tomas <tomasfreire@gmail.com>
2021-08-04 10:46:36 -0300

    thankyou page update

--- src/components/thankyou.js
+++ src/components/thankyou.js
@@ -1,29 +1,30 @@
-import { navigate } from "gatsby"
+import { navigate, Link } from "gatsby"
 import React, { useState } from "react"
 
 const ThankYou = () => {
   const [timeLeft, setTimeLeft] = useState(5)
 
-  const time = () => {
-    let secs = 5
-    const interval = setInterval(() => {
-      setTimeLeft(secs)
-      secs -= 1
-    }, 1000)
-    console.log(timeLeft)
-    if (secs <= 0) {
-      clearInterval(interval)
-      navigate("/")
-    }
-  }  
+  // const time = () => {
+  //   let secs = 5
+  //   const interval = setInterval(() => {
+  //     setTimeLeft(secs)
+  //     secs -= 1
+  //   }, 1000)
+  //   console.log(timeLeft)
+  //   if (secs <= 0) {
+  //     clearInterval(interval)
+  //     navigate("/")
+  //   }
+  // }
 
   return (
-    <div>
+    <div className="section full-height">
       <div>Gracias por tu mensaje!</div>
       {/* <div>
         Tu navegador te redirigirá a la Home nuevamente en... {timeLeft}{" "}
         segundos
       </div> */}
+      <Link to="/">Volver</Link>
     </div>
   )
 }
